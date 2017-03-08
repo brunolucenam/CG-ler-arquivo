5976469a6308982d7caf1d43989b313eff1d2b69
edsonapf <edsonapf@gmail.com>
2017-03-08 13:49:23 -0300

    Add files via upload

--- lerx3d.c
+++ lerx3d.c
@@ -30,16 +30,100 @@ typedef struct listaPonto{
 }tListaPonto;
 
 //protótipos de função
-void inserePonto();
-void insereFace();
-void lerPonto();
-void lerFace();
+//void inserePonto();
+//void insereFace();
+//void lerPonto();
+//void lerFace();
 void lerArquivo();
 //fim dos protótipos
+
+/**Funções para manipular as listas*/
+//Função para iniciar os valores da lista da face
+void criarListaFace(tListaFace *listaF){
+    listaF->inicioFace = NULL;
+    listaF->tamanhoFace = 0;
+}
+
+//Função para iniciar os valores da lista do ponto
+void criarListaPonto(tListaPonto *listaP){
+    listaP->inicioPonto = NULL;
+    listaP->tamanhoPonto = 0;
+}
+
+//Função para verificar se a lista de face está vazia
+int listaFaceVazia(tListaFace listaF){
+    if(listaF.tamanhoFace == 0){
+        return 1;
+    }
+    return 0;
+}
+
+//Função para verificar se a lista de ponto está vazia
+int listaPontoVazia(tListaPonto listaP){
+    if(listaP.tamanhoPonto == 0){
+        return 1;
+    }
+    return 0;
+}
+
+//Função para inserir um elemento na lista da face, sempre no final. Função vai ser int para saber se foi inserido corretamente
+int inserirFace(tListaFace *listaF, int f1, int f2, int f3){
+    tFace *noFace, *fimFace;
+    fimFace = listaF->inicioFace;//esse ponteiro vai percorrer toda lista para inserir no fim
+    //int cont = 1;
+    noFace = malloc(tFace);
+    if(noFace == NULL){//if que verifica se um novo nó foi alocado
+        return 0;
+    }
+    noFace->pt[0] = f1;
+    noFace->pt[1] = f2;
+    noFace->pt[2] = f3;
+    if(listaFaceVazia(*listaF)){
+        noFace->proxFace = NULL;
+        listaF->cabeca = noFace;
+        listaF->tamanhoFace++;
+    }else{
+        while(fimFace->proxFace!=NULL){//percorre até o último índice da lista
+            fimFace = fimFace->proxFace;
+        }
+        noFace->proxFace = NULL;
+        fimFace->prox = noFace;
+        listaF->tamanhoFace++;
+    }
+    return 1;
+}
+
+//Função para inserir um elemento na lista do ponto, sempre no final. Função vai ser int para saber se foi inserido corretamente
+int inserirPonto(tListaPonto *listaP, float xp, float yp, float zp){
+    tPonto *noPonto, *fimPonto;
+    fimPonto = lista->cabeca;
+    noPonto = malloc(tListaPonto);
+    if(noPonto == NULL){
+        return 0;
+    }
+    noPonto->x = xp;
+    noPonto->y = yp;
+    noPonto->z = zp;
+    if(listaPontoVazia(*listaP)){
+        noPonto->proxPonto = NULL;
+        listaP->inicioPonto = noPonto;
+        listaP->tamanhoPonto++;
+    }else{
+        while(fimPonto->proxPonto!=NULL){
+            fim = fimPonto->proxPonto;
+        }
+        noPonto->proxPonto = NULL;
+        fimPonto->proxPonto = noPonto;
+        listaP->tamanhoPonto++;
+    }
+    return 1;
+}
+/**Fim da funções para manipular as listas */
 
 void lerArquivo(FILE *arquivo/*, tListaPonto *listaPonto, tListaFace *listaFace*/){
     char linha[400];
     int i;
+    int cont_aspas = 0;//verificar se foi passado pelas duas aspas
     int qtdeentrou = 0, ent = 1;//serve de nada, só para numerar as faces
 
     //arquivo = fopen("quad.x3d", "r");
@@ -74,12 +158,21 @@ void lerArquivo(FILE *arquivo/*, tListaPonto *listaPonto, tListaFace *listaFace*
                     //printf("entrou if\n");
                     i = i+2;
                 }
+                /**if para verificar se já foi encontrado as duas aspas */
+                if(linha[i] == '\"'){
+                    cont_aspas++;
+                    if(cont_aspas == 2){
+                        //printf("encontrou as %d aspas\n", cont_aspas);
+                        cont_aspas = 0;
+                        break;
+                    }
+                }
 
-            }
-        }
-    }
+            }//fim do for que encontra as faces
+        }//fim do if que procura as até achar o coordindex
+    }//fim do arquivo
     //fclose(arquivo);
-}
+}//fim da função ler arquivo
 
 int main(){
     FILE *f;
